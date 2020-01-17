de04ff05fe127f06568dafd114d27afa4bf1adaf
Sergey <4elovekprostoi19@gmail.com>
2020-01-17 06:26:53 +0200

    adaptivity

--- webakula/js/slider.js
+++ webakula/js/slider.js
@@ -1,7 +1,27 @@
   $('.main').slick({
-  infinite: true,
+  infinite: false,
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
+  // autoplay: true,
+  // autoplaySpeed: 5000,
+
+
+  responsive: [
+    {
+      breakpoint: 1226,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 846,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });

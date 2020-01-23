fbfdf7747796da2063ce6ed6adae3ffef0fcda77
Sergey <4elovekprostoi19@gmail.com>
2020-01-23 03:40:21 +0200

    header changes

---
+++ webakula/js/header_slider.js
@@ -0,0 +1,21 @@
+$('.header_slider').slick({
+  dots: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: false,
+  arrows : false,
+  pauseOnFocus: false,
+  swipe: false,
+  responsive: [
+  {
+    breakpoint: 992,
+    settings: {
+      autoplay: false,
+      slickGoTo:1
+    }
+  }
+  ]
+});

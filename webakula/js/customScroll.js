804acc45fa6b58490541932daa0008ae8e4e466f
Sergey <4elovekprostoi19@gmail.com>
2020-01-25 09:34:44 +0200

    slider image, scroll

---
+++ webakula/js/customScroll.js
@@ -0,0 +1,10 @@
+ (function($){
+ 	$(window).on("load",function(){
+ 		$(".advantages-text").mCustomScrollbar();
+ 	});
+ })(jQuery);
+
+ $(".advantages-text").mCustomScrollbar({
+    axis:"yx", // vertical and horizontal scrollbar
+     theme:"rounded"
+});
--- webakula/js/main.js
+++ webakula/js/main.js
@@ -2,76 +2,104 @@ $(document).ready(function(){
 	$("#phone").mask("+38(099)9999999");
 	$("#email").inputmask("email");
 });
-$(".main-menu .tours").click(function(e) {
-	e.preventDefault();
-	$(".main-menu .tours").removeClass('tour-active');
-	$(this).addClass('tour-active');
-});
 
-$('#footer-form').on('submit', function(event) {
-	if (validateForm()) {
-		event.preventDefault();
-	}
-});
+// $('#footer-form').on('submit', function(event) {
+// 	if (validateForm()) {
+// 		event.preventDefault();
+// 	}
+// });
 
 
 
 
-$('.drop-btn').click(function() {
-	$('.top-menu').toggleClass('show');
-});
 
 
 
+// function validateForm() {
+// 	var name_check = false;
+// 	var email_check = false;
+// 	var phone_check = false;
+// 	var select_check = false;
 
-$(".main-menu .tours").click(function(e) {
-	e.preventDefault();
-	$(".main-menu .tours").removeClass('tour-active');
-	$(this).addClass('tour-active');
-});
+// 	$(".error").removeClass( "error" );
+// 	var el_l= $("#name");
+// 	if ( el_l.val().length < 2 ) {
+// 		name_check = true;
+// 		console.log('name: '+ name_check);
+// 	} 
+// 	$("#name").toggleClass('error', name_check );
+
+// 	var reg= /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
+// 	var el_e= $("#email");
+// 	if (el_e.val()) {
+// 		if (reg.test( el_e.val() )) {
+// 			email_check = false;
+// 		} else {
+// 			email_check = true;
+// 		}
+// 	} else {
+// 		email_check = true;
+// 	}
+// 	$("#email").toggleClass('error', email_check );
+
+// 	var el_l    = $("#phone");
+// 	if ( el_l.val().length < 10 ) {
+// 		phone_check = true;
+// 		console.log('phone: '+ phone_check);
+// 	} 
+// 	$("#phone").toggleClass('error', phone_check );
+
+// 	if ( $("#forms-select").val() == 1 ) {
+// 		select_check = true;
+// 		console.log('select: '+ select_check);
+// 	}
+// 	$("#forms-select").toggleClass('error', select_check );
 
 
-function validateForm() {
-	var name_check = false;
-	var email_check = false;
-	var phone_check = false;
-	var select_check = false;
+// 	return ( name_check || email_check || phone_check || select_check );
+// }
 
-	$(".error").removeClass( "error" );
-	var el_l= $("#name");
-	if ( el_l.val().length < 2 ) {
-		name_check = true;
-		console.log('name: '+ name_check);
-	} 
-	$("#name").toggleClass('error', name_check );
-
-	var reg= /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
-	var el_e= $("#email");
-	if (el_e.val()) {
-		if (reg.test( el_e.val() )) {
-			email_check = false;
-		} else {
-			email_check = true;
+$("#footer-form").validate({
+	rules: {
+		name: {
+			required: true,
+			minlength: 2
+		},
+		email: {
+			required: true,
+			email: true
+		},
+		phone: {
+			required: true,
+			minlength: 12
 		}
-	} else {
-		email_check = true;
+	},
+	messages:{
+		email:{
+			required:"*"
+		},
+		name:{
+			required:"*"
+		},
+		phone:{
+			required:"*"
+		}
 	}
-	$("#email").toggleClass('error', email_check );
-
-	var el_l    = $("#phone");
-	if ( el_l.val().length < 10 ) {
-		phone_check = true;
-		console.log('phone: '+ phone_check);
-	} 
-	$("#phone").toggleClass('error', phone_check );
-
-	if ( $("#forms-select").val() == 1 ) {
-		select_check = true;
-		console.log('select: '+ select_check);
-	}
-	$("#forms-select").toggleClass('error', select_check );
-
-
-	return ( name_check || email_check || phone_check || select_check );
-}
-
+});
+jQuery('#footer-form').validate({
+    showErrors: function(errorMap, errorList) {
+        // Do nothing here
+    },
+    onfocusout: false,
+    onkeyup: false,
+    rules: {
+        email: {
+            required: true
+        }
+    },
+    messages: {
+        email: {
+            required: 'The email is required'
+        }
+    }
+});
